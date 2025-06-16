62a2d6501c811645706774e842f71332e6e848e2
leenagyoung <2000skrud@naver.com>
2025-06-16 21:38:18 +0900

    test
    test

---
+++ src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
@@ -0,0 +1,19 @@
+package com.welab.k8s_backend_user.api.open;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/api/user/v1", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+public class UserController {
+    @GetMapping(value = "/test")
+    public String test(){
+        return "Hello Kubernetes";
+    }
+}
